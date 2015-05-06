12f1b43216dedec3cb8baf3123ef74557e62ee7b
Yong Jun <yongjun@gmail.com>
2015-05-05 23:54:42 -0700

    Switched to a custom table view cell

---
+++ Blocstagram/Blocstagram/MediaTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MediaTableViewCell.h
+//  Blocstagram
+//
+//  Created by Yong Jun on 5/5/15.
+//  Copyright (c) 2015 Yong Jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MediaTableViewCell : UITableViewCell
+
+@end
---
+++ Blocstagram/Blocstagram/MediaTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MediaTableViewCell.m
+//  Blocstagram
+//
+//  Created by Yong Jun on 5/5/15.
+//  Copyright (c) 2015 Yong Jun. All rights reserved.
+//
+
+#import "MediaTableViewCell.h"
+
+@implementation MediaTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
